4f9ab8874fc221e4df9f778d7371f237ef09f7ff
Anson Kan <ansonkan@tutanota.com>
2022-09-28 08:35:11 +0800

    add local env  mongodb url

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -2,10 +2,18 @@ import { StackContext, Api } from '@serverless-stack/resources'
 
 export function MyStack({ stack }: StackContext) {
   const api = new Api(stack, 'api', {
+    defaults: {
+      function: {
+        environment: {
+          MONGODB_URI: process.env.MONGODB_URI ?? '',
+        },
+      },
+    },
     routes: {
       'GET /': 'functions/lambda.handler',
     },
   })
+
   stack.addOutputs({
     ApiEndpoint: api.url,
   })

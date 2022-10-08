76c11afb64453f586a22b04dfbcce026f7bd16ba
Anson Kan <ansonkan@tutanota.com>
2022-10-08 18:03:22 +0800

    setup lefthook

--- frontend/components/BookCard/BookCard.tsx
+++ frontend/components/BookCard/BookCard.tsx
@@ -1,6 +1,6 @@
 import type { Book } from '@lily-bookshop-website/schema'
 
-interface BookCardProps extends Book {}
+type BookCardProps = Book
 
 export const BookCard = (props: BookCardProps): JSX.Element => {
   return <div>Book card... {JSON.stringify(props)}</div>

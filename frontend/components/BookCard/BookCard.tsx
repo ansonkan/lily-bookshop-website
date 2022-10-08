0e478d286d10464a3b2ea6e3aa5b1828b958a448
Anson Kan <ansonkan@tutanota.com>
2022-10-08 22:35:07 +0800

    fill up the home page

--- frontend/components/BookCard/BookCard.tsx
+++ frontend/components/BookCard/BookCard.tsx
@@ -2,6 +2,25 @@ import type { Book } from '@lily-bookshop-website/schema'
 
 type BookCardProps = Book
 
-export const BookCard = (props: BookCardProps): JSX.Element => {
-  return <div>Book card... {JSON.stringify(props)}</div>
+export const BookCard = ({
+  thumbnail,
+  title,
+  subtitle,
+  authors,
+}: BookCardProps): JSX.Element => {
+  return (
+    <div display="flex" flex="row" gap="2">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src={thumbnail} alt={title} width="128" className="self-start" />
+
+      <div display="flex" flex="col" gap="2">
+        <div>
+          <span font="bold">{title}</span>
+          {subtitle && <span> - {subtitle}</span>}
+        </div>
+
+        {authors && <div>Authors: {authors.replaceAll(';', ', ')}</div>}
+      </div>
+    </div>
+  )
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,30 @@
 // Note: This is not a `tsx` file because that doesn't seem to be working with `windi`
 import 'windi.css'
 import '@styles/global.scss'
+import Head from 'next/head'
 
 import { Toolbar } from '@components'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="root">
-      <Toolbar />
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
 
-      <main className="page">
-        <Component {...pageProps} />
-      </main>
+      <div className="root">
+        <Toolbar />
 
-      {/* Footer */}
-    </div>
+        <main className="page">
+          <Component {...pageProps} />
+        </main>
+
+        {/* Footer */}
+      </div>
+    </>
   )
 }
 
